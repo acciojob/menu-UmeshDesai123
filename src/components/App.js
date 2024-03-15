ebdc35766de443f87d3a3eccd1759af9d02fe1d3
UmeshDesai123 <umeshdesai1995@gmail.com>
2024-03-15 12:00:24 +0530

    solution1

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,147 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useState } from "react";
+import './../styles/App.css';
 
+const data = [
+  {
+    id: 1,
+    title: 'buttermilk pancakes',
+    category: 'breakfast',
+    price: 15.99,
+    img: './images/item-1.jpeg',
+    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed`,
+  },
+  {
+    id: 2,
+    title: 'diner double',
+    category: 'lunch',
+    price: 13.99,
+    img: './images/item-2.jpeg',
+    desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+  },
+  {
+    id: 3,
+    title: 'godzilla milkshake',
+    category: 'shakes',
+    price: 6.99,
+    img: './images/item-3.jpeg',
+    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+  },
+  {
+    id: 4,
+    title: 'country delight',
+    category: 'breakfast',
+    price: 20.99,
+    img: './images/item-4.jpeg',
+    desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+  },
+  {
+    id: 5,
+    title: 'egg attack',
+    category: 'lunch',
+    price: 22.99,
+    img: './images/item-5.jpeg',
+    desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+  },
+  {
+    id: 6,
+    title: 'oreo dream',
+    category: 'shakes',
+    price: 18.99,
+    img: './images/item-6.jpeg',
+    desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+  },
+  {
+    id: 7,
+    title: 'bacon overflow',
+    category: 'breakfast',
+    price: 8.99,
+    img: './images/item-7.jpeg',
+    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+  },
+  {
+    id: 8,
+    title: 'american classic',
+    category: 'lunch',
+    price: 12.99,
+    img: './images/item-8.jpeg',
+    desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+  },
+  {
+    id: 9,
+    title: 'quarantine buddy',
+    category: 'shakes',
+    price: 16.99,
+    img: './images/item-9.jpeg',
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
+];
+
+const App = () => {
+  const [items, setItems] = useState(data);
+  const categories = ['All'];
+  data.forEach((item) => {
+    if (!categories.includes(item.category)) {
+      categories.push(item.category);
+    }
+  });
+
+  const categoryFilter = (category) => {
+    
+    if(category === 'All'){
+      setItems(data);
+      return;
+    }
+    const newItems = data.filter((item)=> item.category == category);
+    setItems(newItems);
+  }
+
+  return (
+    <div className="container">
+      <h1>Our Menu</h1>
+      <div className="line"></div>
+      <nav>
+        {
+          categories.map((category) => {
+            return (
+              <div>
+                <label htmlFor={category}>{category.charAt(0).toUpperCase() + category.slice(1)}</label>
+                <input onClick={()=>{categoryFilter(category)}} id={category} type="checkbox"></input>
+              </div>
+            )
+          })
+        }
+      </nav>
+      <div className="cards">
+        {/* <div className="card">
+          <img src="#"></img>
+          <div className="left">
+            <div className="title">
+              <p>buttermilk pancakes</p>
+              <span>$ 15.99</span>
+            </div>
+            <div className="line2"></div>
+            <p>I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed</p>
+          </div>
+        </div> */}
+        {
+          items.map((item) => {
+            return (<div className="card">
+              <img src={item.img}></img>
+              <div className="left">
+                <div className="title">
+                  <p>{item.title}</p>
+                  <span>$ {item.price}</span>
+                </div>
+                <div className="line2"></div>
+                <p>{item.desc}</p>
+              </div>
+            </div>)
+          })
+        }
+
+      </div>
+    </div>
+  )
+}
+
+export default App;
